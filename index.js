9c388f727e1eed447b0f240c38cd2028a6ec8c63
Ibrahim3695 <Ibrahim3595>
2024-06-12 23:25:26 +0100

    Adding edit and checbox fuction

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const inputValue = document.getElementById("input-value")
+const arrangeIcon = document.getElementById("arrange-icon")
+
 const add = () => {
+
+    if (inputValue.value.trim() === "") {
+        alert("Insert your todo")
+        return;
+    }
 
     const cardMap = document.getElementById("card-map")
 
     const inputText = inputValue.value.trim()
+
 
 
     const listen = document.createElement("div")
@@ -20,19 +28,57 @@ const add = () => {
     deleteButton.addEventListener("click", function () {
         listen.remove()
     })
- 
+
+    const edit = document.createElement("button")
+    edit.className = "editBTN"
+    edit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>`
+
+    edit.addEventListener("click", () => {
+
+        // console.log(sennCard)
+        if (edit.textContent === "Save") {
+
+            sennCard.textContent = inputValue.value
+
+            inputValue.value = ""
+
+            edit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>`
+
+
+        } else {
+
+            inputValue.value = sennCard.innerText;
+            edit.textContent = "Save"
+
+        }
+    })
+
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    checkBox.className = "cheks"
+
+    checkBox.addEventListener("click", function () {
+        
+        if (checkBox.checked) {
+            sennCard.classList.add("checked");
+        } else {
+            sennCard.classList.remove("checked");
+        }
+    })
+
+
+
     listen.appendChild(sennCard)
+    
+    listen.appendChild(checkBox)
+    listen.appendChild(edit)
 
     listen.appendChild(deleteButton)
 
     // cardMap.appendChild(listen)
-    cardMap.insertBefore(listen, cardMap.firstChild);
+    cardMap.insertBefore(listen,arrangeIcon, cardMap.firstChild);
 
-    if (inputValue.value === "") {
-        alert("Insert your tod")
-    }else{
-        inputValue.value = null
-    }
+
 
     console.log(inputText)
 
@@ -41,7 +87,7 @@ const add = () => {
 }
 inputValue.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        e.preventDefault(add);
+        e.preventDefault();
         add()
     }
 })
